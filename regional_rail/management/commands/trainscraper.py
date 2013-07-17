ecf7f210635c0ff1a9fcbc82173be94e0d095095
maneesha <ahseenam@gmail.com>
2013-07-16 20:42:02 -0400

    add scraper to manage.py command

--- regional_rail/management/commands/trainscraper.py
+++ regional_rail/management/commands/trainscraper.py
@@ -1,3 +1,6 @@
+#creates manage.py command to run scraper, 
+#integrating it into the django project
+
 from django.core.management.base import BaseCommand, CommandError
 from regional_rail.models import Trains
 import simplejson
@@ -5,6 +8,7 @@ import urllib2
 from datetime import date, datetime, time
 import sys
 import json
+import psycopg2
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -18,17 +22,46 @@ class Command(BaseCommand):
         #convert to a python-readable format (a list of dictionaries)
         #trainstatus = json.loads(septa_json)  #returns a string
         
-        scraped_date = datetime.date(datetime.now()) 
-        scraped_time = datetime.time(datetime.now())
+        scraped_date = str(datetime.date(datetime.now())) 
+        scraped_time = str(datetime.time(datetime.now()))
+        ts = []
         for train in trainstatus:
             train["scraped_date"] = scraped_date
             train["scraped_time"] = scraped_time
             scrapedtrain = Trains()
+
             scrapedtrain.destination = train["dest"]
             scrapedtrain.source = train["SOURCE"]
-            for t in "late, nextstop, scraped_time, service, lat, trainno, scraped_date, lon".split(", "):
-                #setattr(scrapedtrain, t, train[t])
-                print t, type(t), #train[t], type(train[t]),
-            scrapedtrain.save()
+            scrapedtrain.late = train["late"]
+            scrapedtrain.lat = train["lat"]
+            scrapedtrain.lon = train["lon"]
+            scrapedtrain.trainno = train["trainno"]
+            scrapedtrain.service = train["service"]
+            scrapedtrain.nextstop = train["nextstop"]
+            scrapedtrain.scraped_time = train["scraped_time"]
+            scrapedtrain.scraped_date = train["scraped_date"]
 
-        
+
+            #trying to collapse above lines into one loop:
+            #for t in "late, nextstop, scraped_time, service, lat, trainno, scraped_date, lon".split(", "):
+                #setattr(scrapedtrain, t, train[t])
+                #print t, type(t), #train[t], type(train[t]),
+            scrapedtrain.save()
+            #print scrapedtrain
+            #print "----"
+            ts.append(train.values())
+            print scraped_time, scraped_date
+
+        #for t in ts:
+            #print t
+        conn = psycopg2.connect(database = "regional_rail", user = "maneesha", host = "/tmp/")
+        c = conn.cursor()
+        query = "INSERT INTO regional_rail_trains(nextstop, scraped_time, service, destination, lat, trainno, late, source, scraped_date, lon) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+        c.executemany(query, ts)
+        conn.commit()
+        c.close()
+
+
+
+
