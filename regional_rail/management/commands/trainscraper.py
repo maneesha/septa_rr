d1da7b7e52d847046ba85059017dec8ce5795a8b
maneesha <ahseenam@gmail.com>
2013-07-20 16:15:19 -0400

    get scraper command in manage.py to work

--- regional_rail/management/commands/trainscraper.py
+++ regional_rail/management/commands/trainscraper.py
@@ -10,6 +10,25 @@ import sys
 import json
 import psycopg2
 
+
+def longscraper(trainview):
+
+    pass
+
+
+def shortscraper():
+    pass
+
+
+
+
+
+
+
+
+
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print "hello"
@@ -25,10 +44,13 @@ class Command(BaseCommand):
         scraped_date = str(datetime.date(datetime.now())) 
         scraped_time = str(datetime.time(datetime.now()))
         ts = []
+        sts = []
         for train in trainstatus:
+            #complete the data set by adding date / time 
             train["scraped_date"] = scraped_date
             train["scraped_time"] = scraped_time
-            scrapedtrain = Trains()
+            
+            scrapedtrain = Trains()  #scrapedtrain is the model
 
             scrapedtrain.destination = train["dest"]
             scrapedtrain.source = train["SOURCE"]
@@ -40,20 +62,41 @@ class Command(BaseCommand):
             scrapedtrain.nextstop = train["nextstop"]
             scrapedtrain.scraped_time = train["scraped_time"]
             scrapedtrain.scraped_date = train["scraped_date"]
-
-
-            #trying to collapse above lines into one loop:
-            #for t in "late, nextstop, scraped_time, service, lat, trainno, scraped_date, lon".split(", "):
-                #setattr(scrapedtrain, t, train[t])
-                #print t, type(t), #train[t], type(train[t]),
-            scrapedtrain.save()
             #print scrapedtrain
+            #print type(scrapedtrain)
+            #print train
+            #print type(train)
+            #train is a dictionary 
+
+        ######
+        #     #trying to collapse above lines into one loop
+            for t in "dest, SOURCE, late, lat, lon, trainno, service, nextstop, scraped_time, scraped_date".split(", "):
+                setattr(scrapedtrain, t, train[t]) 
+                #arguments are  an object, a string, & an arbitrary value 
+                #For example, setattr(x, 'foobar', 123) is equivalent to x.foobar = 123
+                #so scrapedtrain.t = train[t] .... 
+                #or for example, scrapedtrain.dest = train[dest] and so on
+                #print t, type(t), train[t], type(train[t]), "------"
+                scrapedtrain.save()
+                sts.append(scrapedtrain)
+
+        ######
+
+
+            scrapedtrain.save()
+            print scrapedtrain
             #print "----"
             ts.append(train.values())
-            print scraped_time, scraped_date
+            #print sts
+            #print "---------"
+            #print ts
+            print len(sts), len(ts)
 
-        #for t in ts:
-            #print t
+
+        # print scrapedtrain
+
+        # #for t in ts:
+        #     #print t
         conn = psycopg2.connect(database = "regional_rail", user = "maneesha", host = "/tmp/")
         c = conn.cursor()
         query = "INSERT INTO regional_rail_trains(nextstop, scraped_time, service, destination, lat, trainno, late, source, scraped_date, lon) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
