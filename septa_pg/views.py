2ed518d94a598d4ba290ec63362cbdd92eee5593
maneesha <ahseenam@gmail.com>
2013-10-17 23:35:39 -0400

    date filter works for trainno query

--- septa_pg/views.py
+++ septa_pg/views.py
@@ -21,43 +21,27 @@ def search_x(request):
    
 
 def search(request):
-    #is this the best way to have multiple search terms?
-    #start_date = date(1900, 1, 1)
-    #end_date = date(2015, 12, 31)
-    #kwargs = {(start_date, end_date):'date_and_time__range'}
+    #initialize dates to be extreme range; these can be modified with user input
     start_date = '1900-01-01'
     end_date = "2020-12-31"
 
 
-    #testing passing date submission - dates pass correctly but not yet functional part of app
-    if 'start_date' in request.GET and request.GET['start_date'] and len('start_date') > 1:
+    if 'start_date' in request.GET and request.GET['start_date']:
+    #user input to modify dates 
         start_date = request.GET['start_date']
         end_date = request.GET['end_date']
-        print "**************************"
-        print "original start date: ", start_date
-        #start_date_plus1 = start_date + timedelta(days =1)
-        #print "edited start date: ", start_date_plus1
-        print type(start_date)
-        #print type(start_date_plus1)
-        print "***********************"
         if start_date < end_date:
             message = "You searched for the start date " + start_date + " and the end date " + end_date
         else:
             message = "End date must be after start date."
-        #return HttpResponse(message)
-        #trains_that_day = Trains.objects.filter(date_and_time__range = [start_date, end_date])
-        #return render(request, 'date_results.html', {'start_date':start_date, 'end_date':end_date, 'trains_that_day':trains_that_day})
-        #query_date = 'date_and_time__range = [' + start_date + ',' +  end_date +']'
-
-
 
     if 'search_term_1' in request.GET and request.GET['search_term_1']:
+    #user input to search all trains by lateness, source, destination, and/or next stop
         search_term_1 = request.GET['search_term_1'] 
         field_name_1 = request.GET['field_name_1']      
         filter_type_1 = request.GET['filter_type_1'] 
         query1 = field_name_1 + filter_type_1
         kwargs = {query1:search_term_1}
-        #kwargs[query1] = search_term_1
         search_term_2 = request.GET['search_term_2'] 
         field_name_2 = request.GET['field_name_2'] 
         filter_type_2 = request.GET['filter_type_2'] 
@@ -65,35 +49,39 @@ def search(request):
         #if a second search term was submitted, add it to kwargs        
         if search_term_2:
             kwargs[query2]  = search_term_2
+
+        #create django queryset called trains that filters all data by specified date range
         trains = Trains.objects.filter(date_and_time__range = [start_date, end_date])
+
+        #filter this queryset based on user search terms
         trains = trains.filter(**kwargs).order_by('trainno', 'date_and_time')
+
+        #dictionary to convert search types from plain english to django
         #__icontains and __istartswith are case-insensitive (compare to _contains & _startswith) 
         plain_english = {'__icontains':'contains', '__gt':'greater than', '__lt':'less than', '__istartswith':'starts with', '__exact':'equals'}        
+        
         return render(request, 'search_results.html', {'trains':trains, 'query1':search_term_1, 'field1':field_name_1, 'filter1':plain_english[filter_type_1], 'query2':search_term_2, 'field2':field_name_2, 'filter2':plain_english[filter_type_2], 'start_date': start_date, 'end_date':end_date})
+   
     elif 'train_number_search' in request.GET and request.GET['train_number_search']:
         train_number_search = request.GET['train_number_search']
-        trainno_filter = Trains.objects.filter(trainno__exact=train_number_search)
-        latest_train = trainno_filter.aggregate(Max('late'))
-        lt = latest_train['late__max']
-        return render(request, 'late_results.html', {'latest_train':latest_train, 'lt':lt, 'trainno':train_number_search})
-    #testing passing date submission - dates pass correctly but not yet functional part of app
-    # elif 'start_date' in request.GET and request.GET['start_date']:
-    #     start_date = request.GET['start_date']
-    #     end_date = request.GET['end_date']
-    #     print "**************************"
-    #     print "original start date: ", start_date
-    #     #start_date_plus1 = start_date + timedelta(days =1)
-    #     #print "edited start date: ", start_date_plus1
-    #     print type(start_date)
-    #     #print type(start_date_plus1)
-    #     print "***********************"
-    #     if start_date < end_date:
-    #         message = "You searched for the start date " + start_date + " and the end date " + end_date
-    #     else:
-    #         message = "End date must be after start date."
-    #     #return HttpResponse(message)
-    #     trains_that_day = Trains.objects.filter(date_and_time__range = [start_date, end_date])
-    #     return render(request, 'date_results.html', {'start_date':start_date, 'end_date':end_date, 'trains_that_day':trains_that_day})
+        
+        #create django queryset called latest_train that filters all data by specified date range
+        latest_train = Trains.objects.filter(date_and_time__range = [start_date, end_date])
+
+        #filter this data by trains matching specified train number
+        latest_train = latest_train.filter(trainno__exact = train_number_search)
+
+        #########
+        ##NEED TO GET THE DATE THAT THIS MAX LATE VALUE HAPPENED
+        #########
+
+        #get the max value for late from this query set
+        latest_train_minutes = latest_train.aggregate(Max('late')) #this is an object in the form: {'late__max': 5}
+        #get just the value from that object
+        latest_train_minutes = latest_train_minutes['late__max']
+
+
+        return render (request, 'late_results.html', {'latest_train':latest_train, 'latest_train_minutes':latest_train_minutes, 'train_number_search': train_number_search, 'start_date':start_date, 'end_date':end_date})
 
 
     else:
