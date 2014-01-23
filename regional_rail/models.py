446ada161a4d25f9f438fdaa9f5ca4dc68c7216c
maneesha <ahseenam@gmail.com>
2014-01-23 16:00:46 -0500

    got rid of extra timezone datetime fields

--- regional_rail/models.py
+++ regional_rail/models.py
@@ -3,20 +3,22 @@ from django.db import models
 class Trains(models.Model):
     late = models.IntegerField()
     destination = models.CharField(max_length = 100)
-    scraped_date = models.CharField(max_length = 50)  #does not work if DateField even if septa_django_rr.py sets up as date field
+    scraped_date = models.DateField()
     nextstop = models.CharField(max_length = 100)
     service = models.CharField(max_length = 100)
     source = models.CharField(max_length = 100)
     lat = models.FloatField()
     trainno = models.CharField(max_length = 15)
-    scraped_time = models.CharField(max_length = 50) #does not work if TimeField even if septa_django_rr.py sets up as date field
+    scraped_time = models.TimeField() 
     lon = models.FloatField()
-    date_and_time = models.DateTimeField()
-    scr_date = models.DateField()
-    scr_time = models.TimeField()
 
     def __unicode__(self):
         return u'%d %s %s %s %s %s %s %s %s %s' % (self.late, self.destination, self.scraped_date, self.nextstop, self.service, self.source, self.lat, self.trainno, self.scraped_time, self.lon)
+
+
+
+
+
 
 
 
