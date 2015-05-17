375feb0098532fed62bf285ddc967fffbc4a89f8
maneesha <ahseenam@gmail.com>
2015-05-17 13:59:47 -0400

    add reminder note about url format

--- septa_pg/urls.py
+++ septa_pg/urls.py
@@ -15,6 +15,11 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    #Takes the form:
+    #url(reg_expression, views.xxxxx)
+    #where reg_expression is a regular expression noting the url of the page 127.0.0.1/regular_expression
+    #reg_expression is created here, does not come from somewhere else
+    #in the view is where you tell it what acutal html template file to use
 
     url(r'^search-form/$', views.search_form), 
     url(r'^search/$', views.search),
